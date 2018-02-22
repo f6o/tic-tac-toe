d703917887398d44ea5600e6b18f5c11ff4427cd
f6o <fluffy6h@gmail.com>
2018-02-22 11:57:55 +0000

    Implemented taking turns

--- src/sample_frontend_using_reagent/tutorial.cljs
+++ src/sample_frontend_using_reagent/tutorial.cljs
@@ -2,24 +2,32 @@
     (:require
       [reagent.core :as r]))
 
-(defn square [i state]
-  (let [s (state i)]
+(defn square [i board-info]
+  (let [s ((board-info :board) i)
+        is-x? (deref (board-info :is-x-next?))]
     [:button.square
      {:value i
-      :on-click #(reset! s "X")}
+      :on-click
+      (fn [e]
+
+        (reset! s (if is-x? "X" "O"))
+        (reset! (board-info :is-x-next?)
+                (not is-x?)))}
      @s]))
 
 (defn board []
-  (let [state (vec (map #(r/atom "") (range 9)))
-        status (r/atom "Next Player: X")]
+  (let [board-info
+        {:board (vec (map #(r/atom "") (range 9)))
+         :is-x-next? (r/atom true)}]
     [:div
-     [:div.status @status]
+     [:div.status
+      "TODO: show status"]
      (for [r (range 0 9 3)]
        ^{:key (str "row" r)}
        [:div.row
-        [square r state]
-        [square (+ r 1) state]
-        [square (+ r 2) state]])]))
+        [square r board-info]
+        [square (+ r 1) board-info]
+        [square (+ r 2) board-info]])]))
 
 (defn game []
   [:div.game
