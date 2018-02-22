34490f5ceca93a78fb422ac605b46d193db657d8
f6o <fluffy6h@gmail.com>
2018-02-22 11:45:38 +0000

    more smarter

--- src/sample_frontend_using_reagent/tutorial.cljs
+++ src/sample_frontend_using_reagent/tutorial.cljs
@@ -9,26 +9,23 @@
       :on-click #(reset! s "X")}
      @s]))
 
-(defn board-row [start state]
-  [:div.board-row
-   [square start state]
-   [square (+ start 1) state]
-   [square (+ start 2) state]])
-
 (defn board []
   (let [state (vec (map #(r/atom "") (range 9)))
         status (r/atom "Next Player: X")]
     [:div
      [:div.status @status]
-     [board-row 0 state]
-     [board-row 3 state]
-     [board-row 6 state]]))
+     (for [r (range 0 9 3)]
+       ^{:key (str "row" r)}
+       [:div.row
+        [square r state]
+        [square (+ r 1) state]
+        [square (+ r 2) state]])]))
 
 (defn game []
   [:div.game
-   [:div.game-board [board]]
+   [:div.game-board
+    [board]]
    [:div.gameinfo
     [:div "status"]
     [:ol
-     ^{:key 1} [:li "todo"]]]
-   ])
+     ^{:key 1} [:li "todo"]]]])
