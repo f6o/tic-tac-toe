150f75b1021bb83a3254ec19b4fadd50907aefb5
f6o <fluffy6h@gmail.com>
2018-02-27 00:33:18 +0000

    show a winner

--- src/sample_frontend_using_reagent/tutorial.cljs
+++ src/sample_frontend_using_reagent/tutorial.cljs
@@ -2,6 +2,23 @@
     (:require
       [reagent.core :as r]))
 
+(defn decide-board-winner [board]
+  (some (fn [line]
+          (every? (fn [i]
+                      (and
+                       (not (clojure.string/blank? (deref (board i))))
+                       (= (deref (board i))
+                          (deref (board (first line))))))
+                  (rest line)))
+        '((0 1 2)
+          (3 4 5)
+          (6 7 8)
+          (0 3 6)
+          (1 4 7)
+          (2 5 8)
+          (0 4 8)
+          (2 4 6))))
+
 (defn square [i board-info]
   (let [s ((board-info :board) i)
         is-x? (deref (board-info :is-x-next?))]
@@ -10,19 +27,25 @@
       :on-click
       (fn [e]
         (reset! s (if is-x? "X" "O"))
-        (reset! (board-info :is-x-next?)
-                (not is-x?)))}
+        (if (or
+             (deref (board-info :done))
+             (decide-board-winner (board-info :board)))
+          (reset! (board-info :done) true)
+          (reset! (board-info :is-x-next?) (not is-x?))))}
      @s]))
 
 (defn next-player [board-info]
   [:div.status
-   "Next Player:"
+   (if (deref (board-info :done))
+     "Winner:"
+     "Next Player:")
    [:span.name
     (if (deref (board-info :is-x-next?)) "X" "O")]])
 
 (defn board []
   (let [board-info
         {:board (vec (map #(r/atom "") (range 9)))
+         :done (r/atom false)
          :is-x-next? (r/atom true)}]
     [:div
      [next-player board-info]
