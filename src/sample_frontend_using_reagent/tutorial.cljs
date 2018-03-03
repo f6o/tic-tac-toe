91b22b7a640d323adc2c763a6c7b316956fd5620
f6o <fluffy6h@gmail.com>
2018-03-03 10:58:29 +0000

    refine whole

--- src/sample_frontend_using_reagent/tutorial.cljs
+++ src/sample_frontend_using_reagent/tutorial.cljs
@@ -5,38 +5,39 @@
 (defn board-filled? [board]
   (every? #(re-matches #"[OX]" (deref %)) board))
 
-(defn decide-board-winner [board]
-  (some (fn [line]
-          (every? (fn [i]
-                      (and
-                       (not (clojure.string/blank? (deref (board i))))
-                       (= (deref (board i))
-                          (deref (board (first line))))))
-                  (rest line)))
-        '((0 1 2)
-          (3 4 5)
-          (6 7 8)
-          (0 3 6)
-          (1 4 7)
-          (2 5 8)
-          (0 4 8)
-          (2 4 6))))
+(defn won? [board]
+  (some
+   (fn [line]
+     (every? (fn [i]
+               (and
+                (not (clojure.string/blank? (deref (board i))))
+                (= (deref (board i))
+                   (deref (board (first line))))))
+             (rest line)))
+   '((0 1 2) (3 4 5) (6 7 8)
+     (0 3 6) (1 4 7) (2 5 8)
+     (0 4 8) (2 4 6))))
+
+(defn click-square [s board-info]
+  (let [is-x? (deref (board-info :is-x-next?))
+        result (deref (board-info :result))]
+    (js/console.log (clj->js board-info))
+    (when (and (clojure.string/blank? @s)
+               (= result :in-play))
+      (reset! s (if is-x? "X" "O"))
+      (reset! (board-info :is-x-next?) (not is-x?))
+      (reset!
+       (board-info :result)
+       (cond
+         (won? (board-info :board)) :done
+         (board-filled? (board-info :board)) :draw
+         :else :in-play)))))
 
 (defn square [i board-info]
-  (let [s ((board-info :board) i)
-        is-x? (deref (board-info :is-x-next?))]
+  (let [s ((board-info :board) i)]
     [:button.square
      {:value i
-      :on-click
-      (fn [e]
-        (when (not (board-filled? (board-info :board)))
-          (if (not (deref (board-info :done)))
-            (reset! s (if is-x? "X" "O")))
-          (if (or
-               (deref (board-info :done))
-               (decide-board-winner (board-info :board)))
-            (reset! (board-info :done) true)
-            (reset! (board-info :is-x-next?) (not is-x?)))))}
+      :on-click #(click-square s board-info)}
      @s]))
 
 (defn empty-board []
@@ -44,26 +45,24 @@
 
 (defn draw-board []
   (vec
-   (map
-    #(r/atom %)
-    (list
-     "O" "X" "O"
-     "O" "X" "X"
-     "X" "O" ""))))
+   (map #(r/atom %)
+        (list
+         "O" "X" "O"
+         "O" "X" "X"
+         "X" "O" ""))))
 
 (defn create-new-board []
   {:board (empty-board)
-   :done (r/atom false)
+   :result (r/atom :in-play)
    :is-x-next? (r/atom true)})
-
-(defn show-player [board-info]
-  (if (deref (board-info :is-x-next?)) "X" "O"))
 
 (defn show-status [board-info]
   [:div.status
-   (cond (board-filled? (board-info :board)) "Draw Game"
-         (deref (board-info :done)) (str "Winner:" (show-player board-info))
-         :else (str  "Next Player:" (show-player board-info)))])
+   (case (deref (board-info :result))
+     :done (str "Winner:"
+                (if (deref (board-info :is-x-next?)) "O" "X"))
+     :draw "Draw Game"
+     (str "Next Player: " (if (deref (board-info :is-x-next?)) "X" "O")))])
 
 (defn board []
   (let [board-info (create-new-board)]
@@ -78,8 +77,7 @@
 
 (defn game []
   [:div.game
-   [:div.game-board
-    [board]]
+   [:div.game-board [board]]
    [:div.gameinfo
     [:div "status"]
     [:ol
