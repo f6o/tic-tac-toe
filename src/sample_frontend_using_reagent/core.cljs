facc8bc6fbf6231ff382518c2f13ab15d5ff71df
f6o <fluffy6h@gmail.com>
2018-03-04 09:21:06 +0000

    remove unnecessary codes

--- src/sample_frontend_using_reagent/core.cljs
+++ src/sample_frontend_using_reagent/core.cljs
@@ -1,52 +1,10 @@
 (ns sample-frontend-using-reagent.core
   (:require
-   [sample-frontend-using-reagent.tutorial :as tut]
-   [reagent.core :as r]))
-
-;; -------------------------
-;; Views
-
-(def todo (r/atom (list "Cutting edges"
-                        "Buying the milk"
-                        "Cleaning seats")))
-
-(defn del-link []
-  [:a.del
-   {:href "#" :on-click (fn [self] (print self))}
-   "-"])
-
-(defn todo-item [item]
-  [:li ^{:key item}
-   [:span.item item]
-   [del-link]])
-
-(defn todo-component []
-  [:ul
-   (for [item @todo]
-     [todo-item item])])
-
-(def global-counter (r/atom 0))
-
-(defn count-up-link [counter]
-  [:a.up
-   {:href "#" :on-click (fn [] (swap! counter inc))}
-   "add"])
-
-(defn counter-component [counter]
-  [:div
-   "Now:" [:span.count @counter]
-   [count-up-link counter]])
-
-;; [:div
-;;  [todo-component]
-;;  [counter-component (r/atom 0)]]
-
-;; -------------------------
-;; Initialize app
+   [sample-frontend-using-reagent.tutorial :as tut]))
 
 (defn mount-root []
-  (r/render
-   [tut/game (tut/create-new-state) (r/atom [])]
+  (reagent.core/render
+   [tut/game (tut/create-new-state) (reagent.core/atom [])]
    (.getElementById js/document "app")))
 
 (defn init! []
