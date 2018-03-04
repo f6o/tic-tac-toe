fcecb506ef7e37051424b801a305a19c3cac2be6
f6o <fluffy6h@gmail.com>
2018-03-04 02:18:07 +0000

    update project meta data

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject sample-frontend-using-reagent "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject sample-frontend-using-reagent "1.0.0"
+  :description "tick-tac-toe from the React tutorial"
+  :url "https://github.com/f6o/tic-tac-toe/blob/master/project.clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
